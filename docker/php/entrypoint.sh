db03469b1ca1c14f6bcdf10d140339c840aec091
saraHH1417 <sara.afzar@gmail.com>
2023-01-18 23:50:35 +0330

    feat: set up docker

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sleep 20 && cd /usr/src/app && chmod o+w /usr/src/app/storage/ -R  && php artisan key:generate && php artisan migrate  && php artisan db:seed &&  php artisan optimize
+exec $@
